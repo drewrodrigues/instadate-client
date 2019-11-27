ead2e6cec63c3211acda52d8e4103c6d047144d6
Drew <thesimpledev@gmail.com>
2019-11-26 23:11:12 -0800

    Send session token in auth through axios header

--- config/axios.js
+++ config/axios.js
@@ -1,11 +1,26 @@
 import axios from "axios";
 import getEnvVars from "../environment";
+import { AsyncStorage } from 'react-native';
+
 const { apiUrl } = getEnvVars();
 
-export default axios.create({
-  baseURL: apiUrl,
-  headers: {
-    Accept: 'application/json'
-  },
-  withCredentials: true
-});
+// pull cookie from async storage then send it with request
+export default async (params) => {
+  const token = await _getToken();
+  return axios.create({
+    baseURL: apiUrl,
+    headers: {
+      Accept: 'application/json',
+      Authorization: token
+    },
+    withCredentials: 'omit'
+  })(params);
+}
+
+// helpers
+
+// pull cookie from Async storage
+const _getToken = async () => {
+  const session = await AsyncStorage.getItem('@session');
+  return JSON.parse(session).session_token;
+};
