550bdd097ca81f584668a8549b242efd95ddcdb1
Drew <thesimpledev@gmail.com>
2019-11-30 06:34:32 -0800

    Fix receive dates to work correctly with API

--- features/dates/_action.js
+++ features/dates/_action.js
@@ -1,10 +1,10 @@
 import axios from '../../config/axios';
 
-export const RECEIVE_DATE = 'RECEIVE_DATE';
+export const RECEIVE_DATES = 'RECEIVE_DATES';
 
-const receiveDate = date => ({
-  type: RECEIVE_DATE,
-  date
+const receiveDate = dates => ({
+  type: RECEIVE_DATES,
+  dates
 });
 
 export const createDate = instadate => dispatch => {
@@ -16,26 +16,34 @@ export const createDate = instadate => dispatch => {
     data: { instadate }
   })
   .then(res => {
-    dispatch(receiveDate(res.data));
     dispatch({ TYPE: 'CREATE_DATE_SUCCESS' });
+
+    dispatch(receiveDate(res.data));
     return Promise.resolve(res.data);
   })
   .catch(err => {
     dispatch({ type: 'CREATE_DATE_FAIL' });
+
     return Promise.reject(err.response.data);
   });
 };
 
 export const getDate = creator_id => dispatch => {
+  dispatch({ type: 'GET_DATE_START' });
+
   return axios({
     method: 'get',
     url: `/instadates?creator_id=${creator_id}`
   })
   .then(res => {
+    dispatch({ type: 'GET_DATE_SUCCESS', response: res.data });
+
     dispatch(receiveDate(res.data.dates));
     return Promise.resolve(res.data.dates);
   })
   .catch(err => {
+    dispatch({ type: 'GET_DATE_FAIL', error: err.response.data });
+
     return Promise.reject(err.response.data);
   });
 };
--- features/dates/_reducer.js
+++ features/dates/_reducer.js
@@ -1,11 +1,11 @@
-import { RECEIVE_DATE } from "./_action";
+import { RECEIVE_DATES } from "./_action";
 
-export default function(oldState = null, action) {
+export default function(oldState = [], action) {
   Object.freeze(oldState);
 
   switch (action.type) {
-    case RECEIVE_DATE:
-      return Array.isArray(action.date) ? action.date[0] : action.date;
+    case RECEIVE_DATES:
+      return action.dates;
     default:
       return oldState;
   }
