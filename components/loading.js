fd71d05a2fb45d475574cae8e9c2b93cffebb23e
Drew <thesimpledev@gmail.com>
2019-11-30 11:47:42 -0800

    Add loading component

---
+++ components/loading.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import LoadingGif from '../assets/loading.gif';
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Image source={LoadingGif} style={styles.gif} />
+      <Text style={styles.text}>Loading...</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gif: {
+    height: 150,
+    width: 150
+  },
+  text: {
+    color: '#777',
+    fontSize: 14,
+  }
+});
